acbafe0aca05ac3f0fb57ca0b3754df4dd706563
unknown <raffaele.pavone98@gmail.com>
2023-04-14 15:38:56 +0200

    fixed comments

--- src/middlewares/auth.guard.js
+++ src/middlewares/auth.guard.js
@@ -18,8 +18,11 @@ async function basicAuth(req, res, next) {
 
   // verify auth credentials
   const base64Credentials = req.headers.authorization.split(' ')[1];
+  // decode base64 credentials
   const credentials = Buffer.from(base64Credentials, 'base64').toString('utf8');
+  // split credentials into username and password
   const [username, password] = credentials.split(':');
+  // authenticate user
   const user = await authService.authenticate({ username, password });
   if (!user) {
     return res.status(401).json({ message: 'Invalid Credentials' });
